aca218f6551d7f4990ea85c08abf2962317a80cf
alissonpeixer <alissonpeixer4@gmail.com>
2022-12-17 19:06:07 -0300

    isTest

--- src/pages/api/socket.js
+++ src/pages/api/socket.js
@@ -1,20 +1,17 @@
 import { Server } from 'socket.io'
 
 const SocketHandler = (req, res) => {
-  if (res.socket.server.io) {
-    console.log('Socket is already running')
-  } else {
-    console.log('Socket is initializing')
-    const io = new Server(res.socket.server)
-    res.socket.server.io = io
+  const io = new Server(res.socket.server)
+  res.socket.server.io = io
 
-    io.on('connection', socket => {
-      socket.on('input-change', msg => {
-        socket.broadcast.emit('update-input', msg)
-      })
+  io.on('connection', (socket) => {
+    socket.on('input-change', (msg) => {
+      io.emit('update-input', msg)
     })
-  }
-  res.end()
+  })
+
+  console.log('Setting up socket')
+  res.socket.end()
 }
 
 export default SocketHandler
