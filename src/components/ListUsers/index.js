a73b52d7aedfbf416d759ab0af4f7777d1f40dbe
Alisson <alisson@pop-os.localdomain>
2022-12-22 11:15:43 -0300

    feat: oline user list and remove, user left

--- src/components/ListUsers/index.js
+++ src/components/ListUsers/index.js
@@ -11,42 +11,47 @@ export const ListUsers = ({ socket, users, setUsers }) => {
 
 
   useEffect(() => {
-    socket.on("userLogin", usersApi => {
-      usersApi.map(userApi => {
-        setUsers(prevState => [
-          ...prevState,
-          {
-            username: userApi.username,
-            socketId: userApi.socketId
-          }
-        ])
-      })
-    })
 
+    if (users.length <= 1) {
+      socket.on('userLogin', usersApi => {
+        usersApi.map(user => {
+          setUsers(prevState => [
+            ...prevState,
+            user
+          ])
+        })
+      })
+    }
 
     socket.on('notifyUserJoined', (userApi) => {
+      console.log(userApi)
       setUsers(prevState => [
         ...prevState,
-        {
-          username: userApi.username,
-          socketId: userApi.socketId
-        }
+        userApi.data
       ])
     })
 
-    socket.on('notifyUserLeft', (user) => {
+    socket.on('notifyUserLeft', async (userApi) => {
+
+      setUsers(prevState => (
+        prevState.filter((value, i) => value.socketId !== userApi.socketId)
+      ))
+
 
     })
 
     return () => {
 
       socket.off('notifyUserJoined')
-      socket.off('notifyUserLeft')
+      socket.off('userLogin')
     }
   }, [socket])
 
 
+  const filterUser = (userApi) => {
 
+  }
+  console.log(users)
   return (
     <main className="flex-1 bg-white bg-opacity-10 backdrop-blur-xl rounded-2xl h-full hidden xl:block p-5">
 
--- src/utils/sockets.js
+++ src/utils/sockets.js
@@ -1,12 +1,31 @@
+
+
 const users = []
 
+
+const removeUser = (socket) => {
+  console.log(`- ${socket.id} LEFT`)
+  let removeUser
+  users.map((user, id) => {
+    if (user.socketId === socket.id) {
+      users.splice(id)
+      removeUser = user
+    }
+
+
+  })
+
+
+  return removeUser
+}
+
 export default (io, socket) => {
-  console.log('LISTA DE USERS')
-  console.log(users)
+
 
   socket.on('sendMensage', data => {
     // console.log(data)
     socket.emit('sendStatusCheck')
+
     socket.broadcast.emit('recebMensage', {
       ...data,
       you: false
@@ -15,10 +34,15 @@ export default (io, socket) => {
 
 
   socket.on('userJoined', data => {
-    users.push(data)
+
 
     // for user
     socket.emit('userLogin', users)
+
+
+    users.push(data)
+
+
 
     // for all users connected
     socket.broadcast.emit('notifyUserJoined', {
@@ -26,18 +50,11 @@ export default (io, socket) => {
     })
   })
 
-  socket.on('disconnect', data => {
-    // console.log(socket)
+  socket.on('disconnect', async data => {
 
-    users.map((user, id) => {
-      if (user.socketId === socket.id) {
-        users.splice(id)
-        socket.broadcast.emit('notifyUserLeft', user)
-      }
-
-
-    })
-
+    const user = await removeUser(socket)
+    console.log(user)
+    socket.broadcast.emit('notifyUserLeft', user)
 
   })
 
